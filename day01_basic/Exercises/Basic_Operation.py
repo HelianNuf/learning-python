1dbc2522851afcec8022ffd7ded6e3b5c956d8d9
HelianNuf <fadhilah2578@gmail.com>
2026-01-03 21:58:22 +0800

    Add files via upload

---
+++ day01_basic/Exercises/Basic_Operation.py
@@ -0,0 +1,16 @@
+## Math Basic Operation
+print("=== Basic Operation ===")
+
+a = 20
+b = 5
+
+print("\nfirst num :", a)
+print("second num :", b)
+
+print("\nsum :", a + b)
+print("subtraction :", a - b)
+print("multiplication :", a * b)
+print("division :", a / b)
+print("modulus :", a % b)
+print("exponent :", a ** b)
+print("integer division :", a // b)
