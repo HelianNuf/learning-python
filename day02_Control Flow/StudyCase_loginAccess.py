664cc0a5eb5b7d04968106d7d9b11ffed0f84763
HelianNuf <fadhilah2578@gmail.com>
2026-01-03 21:32:15 +0800

    Delete day02_Control Flow directory

--- day02_Control Flow/StudyCase_loginAccess.py
+++
@@ -1,9 +0,0 @@
-# The basic login system uses logical operators
-
-user = input("Username: ")
-pw = input("Password: ")
-
-if user == "admin" and pw == "123":
-    print("Login Success")
-else:
-    print("input invalid")
