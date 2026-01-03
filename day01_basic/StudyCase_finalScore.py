01980629f9417b34786329a0630252902b47e7c9
HelianNuf <fadhilah2578@gmail.com>
2026-01-03 22:17:25 +0800

    Refactor Day 01: Clarify Study Cases

--- day01_basic/StudyCase_finalScore.py
+++ day01_basic/StudyCase_finalScore.py
@@ -1,7 +1,10 @@
+# Study Case: Calculation average score for final grades.
+   
 print("=== FINAL SCORE CALCULATION ===")
 task = 80
 exam = 79
 final_exam = 87
 
 print("Assignment score :", task, "\nExam score       :", exam, "\nFinal exam score :", final_exam)
+
 print("\nFinal Score :", (task + exam + final_exam)/3)
