2a46d78ade1e051b5c9e93bf497bb0e9b6cb83ca
HelianNuf <fadhilah2578@gmail.com>
2026-01-04 18:41:47 +0800

    Delete day03_loop/Error_handling_preview.py

--- day03_loop/Error_handling_preview.py
+++
@@ -1,8 +0,0 @@
-# Basic error handling preview
-# Used to support case study stability
-
-try:
-    age = int(input("Enter your age: "))
-    print("Your age is:", age)
-except ValueError:
-    print("Invalid input. Please enter a number.")
