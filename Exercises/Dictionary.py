4a54345426fa5b52594a11cc0153885d8d84e2ee
HelianNuf <fadhilah2578@gmail.com>
2026-01-05 23:53:20 +0800

    Day 04: Data Structure Basic

---
+++ Exercises/Dictionary.py
@@ -0,0 +1,12 @@
+# dictionary
+
+# print student biodata
+
+biodata = {
+    "Nama" : "Nur Fadhilah",
+    "Umur" : "20",
+    "Jurusan" : "Informatika"
+}
+
+for key, value in biodata.items():
+    print(key, ":", value)
